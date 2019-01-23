c1bfb66eb1a538a69c634f87e7dc3c3f2141c066
footios <footios76@gmail.com>
2019-01-23 17:07:52 +0200

    update FullPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -23,15 +24,25 @@ class Blog extends Component {
       this.setState({ posts: udatedPosts });
     });
   }
+
+  postSelectedHandler(id) {
+    this.setState({ selectedPostId: id });
+  }
+
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} author={post.author} />
+      <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(id)}
+      />
     ));
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
