0e87700b8027f40b33129e4ddda7598bb0844cc2
footios <footios76@gmail.com>
2019-01-24 12:33:47 +0200

    added interceptors in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,66 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
+
+// In case you want to execute code globaly:
+// You can do it with axios with the help of so-called interceptors,
+// these are functions you can define globally which will be executed
+// for every request leaving your app,
+// and every response returning into it.
+// This is especially useful for example for setting some common headers like authorization header maybe
+// or for responses
+// if you want to log responses or want to handle errors globally.
+
+// index.js is the most global file
+// interceptors is a specila obj
+// here we access the 'request' obj
+// and we add 'use' to register a new interceptor.
+// Thast interceptor takes a func which receives the request (config).
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    //   In your interceptor function here,
+    //   you need to always return the request or the request config
+    //   otherwise you're blocking the request.
+    //   Now of course you can also edit the request config before you return it,
+    //   that's the idea behind the interceptor,
+    //   you can add headers, do stuff like that.
+    return request;
+  },
+  // we can add a second function which handles any errors
+  error => {
+    // So here we can log an error like this
+    console.log(error);
+    // we should also return promise-reject-error here though
+    // so that we still forward it to our request as
+    // we wrote it in a component where we can handle it again
+    // with the catch method.
+    // This make sense if you
+    // have some local task you want to do
+    // like show something on the UI but also globally,
+    // or you want to log it in the log file
+    // which you send to a server or something like that.
+
+    return Promise.reject(error);
+  }
+);
+
+// We can also add an interceptor to handle responses though, we do in the
+// same way as you do it for the request
+// but we use the response object on the interceptors object.
+
+axios.interceptors.response.use(
+  response => {
+    console.log(response);
+    return response;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
